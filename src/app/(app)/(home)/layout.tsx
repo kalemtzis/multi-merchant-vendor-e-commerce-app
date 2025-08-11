7b8f092c626309ea7a151f984bc481345eec8e5e
kalemtzis <kalemtzis@csd.auth.gr>
2025-08-11 15:51:47 +0300

    create basic skeleton and trpc setup

---
+++ src/app/(app)/(home)/layout.tsx
@@ -0,0 +1,33 @@
+import Footer from "@/app/modules/home/ui/footer";
+import Navbar from "@/app/modules/home/ui/navbar";
+import {
+  SearchFilters,
+  SearchFiltersSkeleton,
+} from "@/app/modules/home/ui/search-filters";
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Suspense } from "react";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const Layout = async ({ children }: Props) => {
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(trpc.categories.getMany.queryOptions());
+
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Navbar />
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <Suspense fallback={<SearchFiltersSkeleton />}>
+          <SearchFilters />
+        </Suspense>
+      </HydrationBoundary>
+      <main className="flex-1 bg-[#F4F4F0]">{children}</main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
